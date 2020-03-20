3bf1a121bfaccf97c7debe8477edd553687f0df5
Klayton Cavalcante <neemensis@gmail.com>
2020-03-20 12:18:45 -0300

    First commit
    Creating the basic project.

---
+++ EFCoreInMemoryDemo.Web/Program.cs
@@ -0,0 +1,35 @@
+﻿using EFCoreInMemoryDemo.Web.DataContext;
+using EFCoreInMemoryDemo.Web.Models;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EFCoreInMemoryDemo.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //1. Get the IWebHost which will host this application.
+            var host = CreateWebHostBuilder(args).Build();
+
+            //2. Find the service layer within our scope.
+            using (var scope = host.Services.CreateScope())
+            {
+                //3. Get the instance of BoardGamesDBContext in our services layer
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<BoardGamesDBContext>();
+
+                //4. Call the DataGenerator to create sample data
+                DataGenerator.Initialize(services);
+            }
+
+            //Continue to run the application
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
